3ee8ada68e96b4e730972fa923ee4a879b780559
Abdullah Ayaz <106861398+abd529@users.noreply.github.com>
2023-05-11 03:59:10 -0700

    Revert "update 1"
    This reverts commit fe904007b4d6e58cbf62ead9f8b4204c3d973410.

--- android/app/src/main/kotlin/com/example/wheel_of_life/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wheel_of_life/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.wheel_of_life
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
